c4adf034a54e67b5f04aaa1b4ae7a8098cb668bc
Khanh LeViet <khanhlvg@google.com>
2016-10-13 12:03:04 +0900

    Refactored Node.js server code and README following nivco@ review
    https://github.com/firebase/custom-auth-samples/pull/1

--- Line/README.md
+++ Line/README.md
@@ -9,28 +9,27 @@ This sample shows how to Sign in Firebase using [LINE Login](https://developers.
  1. Follow the instruction in [LINE Developers document](https://developers.line.me/line-login/overview) to setup a LINE business account.
  1. After finished setting up your channel, follow the document to integrate LINE SDK in your [iOS](https://developers.line.me/ios/overview) and [Android](https://developers.line.me/android/overview) app. It should include:
   * Open **Technical Configuration** page of your LINE channel, and update the following values:
-	   * iOS Bundle ID: `com.google.firebase.LINELoginDemo`
-	   * Android Package Name: `com.google.firebase.linelogindemo`
-	   * Android Package Signature: The SHA1 value of your keystore, without semicolons
+    * iOS Bundle ID: `com.google.firebase.LINELoginDemo`
+    * Android Package Name: `com.google.firebase.linelogindemo`
+    * Android Package Signature: The SHA1 value of your keystore, without semicolons
   * Download the LINE SDK for iOS and Android from LINE developers site and add them to your apps. The latest version of LINE SDK may have different API with that used to create the sample app, so you may need to make some changes to the sample apps. LINE SDK versions being used in building the sample apps are:
-       *  iOS: **3.2.1**
-       *  Android: **3.1.20** 
+    *  iOS: **3.2.1**
+    *  Android: **3.1.20** 
   * Update your Channel ID to your apps
-	   * iOS: Update `<your_channel_id>` in `LineAdapter.plist`
-	   * Android: Update `<your_channel_id>` in `app/build.gradle` 
-	   * Server: Update `<your_channel_id>` in `app.js` 
+    * iOS: Update `<your_channel_id>` in `LineAdapter.plist`
+    * Android: Update `<your_channel_id>` in `app/build.gradle` 
+    * Server: Update `<your_channel_id>` in `config.js` 
 
 ### Firebase app creation and setup
 
  1. Create a Firebase project using the [Firebase Developer Console](https://console.firebase.google.com).
  1. **For the iOS app:** Go to **Firebase Console > Overview > Add Firebase to your iOS app** and create an app with the Bundle ID of `com.google.firebase.LINELoginDemo`. This will trigger your browser to download a `GoogleService-Info.plist` config file. Add this config file to your sample iOS app, then run `pod install` to make download necessary CocoaPods dependencies.
  1. **For the Android app:** Go to **Firebase Console > Overview > Add Firebase to your Android app** and create an app with the Package name of `com.google.firebase.linelogindemo`. This will trigger your browser to download a `google-services.json` config file. Add this config file to your sample Android app.
- 	* If you have problem creating a your Android app, try changing the Android app's package name to your own value (e.g. `com.yourdomain.linelogindemo`) and try again. Remember to update the package name inside your Android app and in your LINE Channel's **Technical Configuration** page as well.
- 1. **For the server:** Go to **Firebase Console > Settings > General**. Copy the `Web API Key` value and paste it in `server/app.json` as the value of `myFirebaseApiKey` in lieu of the placeholder.
-
+  * If you have problem creating a your Android app, try changing the Android app's package name to your own value (e.g. `com.yourdomain.linelogindemo`) and try again. Remember to update the package name inside your Android app and in your LINE Channel's **Technical Configuration** page as well.
+ 
 Create and provide a Service Account's keys:
  1. Create a Service Accounts file as described in the [Server SDK setup instructions](https://firebase.google.com/docs/server/setup#add_firebase_to_your_app).
- 1. Save the Service Account credential file as `service-account.json`
+ 1. Save the Service Account credential file as `service-account.json` and copy it to `server/`.
 
 ## Run the sample
 
@@ -53,9 +52,9 @@ Build the sample iOS and Android app and run them on your devices. Those sample 
 ## Deploy the server sample
 
 You can deploy the sample's server on App Engine Flex. For this:
- - Download and setup the [gcloud](https://cloud.google.com/sdk/) CLI.
- - Make sure Billing is enabled in your Google project and that Google Compute Instance is also enabled.
- - Run `cd server & gcloud app deploy`
+  * Download and setup the [gcloud](https://cloud.google.com/sdk/) CLI.
+  * Make sure Billing is enabled in your Google project and that Google Compute Instance is also enabled.
+  * Run `cd server & gcloud app deploy`
 
  Your app will be available under your App Engine URL: `https://<project-id>.appspot.com`
 
--- Line/server/app.js
+++ Line/server/app.js
@@ -15,132 +15,83 @@
 // [START app]
 'use strict';
 
-var firebase = require('firebase');
-var request = require('request');
-var express = require('express');
-var bodyParser = require('body-parser');
+// Modules imports
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-/*** Configuration BEGIN ***/
-var MY_LINE_CHANNEL_ID = '<your_channel_id>';
-var MY_FIREBASE_API_KEY = '<your_firebase_api_key>';
-/*** Configuration END ***/
-
-//Generate Firebase Custom Auth token
-function generateFirebaseToken(lineMid) {
-  var firebaseUid = 'line:' + lineMid;
-  var additionalClaims = {
-    provider: 'LINE'
-  };
-  return firebase.auth().createCustomToken(firebaseUid);
-}
-
-function updateUserProfile(lineAccessToken, firebaseToken, mid, callback) {
-
-  var options = {
-    url: 'https://api.line.me/v1/profile',
-    headers: {
-      'Authorization': 'Bearer ' + lineAccessToken
-    }
-  };
-
-  request(options, function(error, response, body) {
-    if (!error) {
-      var lineObj = JSON.parse(body);
-
-      console.log(lineObj);
-
-      // Create a Firebase app we'll use to authenticate as the user.
-      var fbAppID = (new Date).getTime() + ":" + mid; //An unique ID for Firebase user app
-      const userApp = firebase.initializeApp({
-        apiKey: MY_FIREBASE_API_KEY,
-      }, fbAppID);
-
-      // Authenticate as the user.
-      userApp.auth().signInWithCustomToken(firebaseToken).then(user => {
-        console.log(user.displayName);
-
-        if (user.displayName == null) {
-          user.updateProfile({
-            displayName: lineObj.displayName,
-            photoURL: lineObj.pictureUrl
-          });
-        }
-
-        return Promise.all(tasks)
-          .then(() => {
-            userApp.delete();
-            callback();
-          });
-      }).catch(() => {
-        userApp.delete();
-        callback();
-      });
-    } else {
-      console.log('Error fetching LINE profile for mid = ', mid);
-      callback();
-    }
-  });
-
-}
-
+// Firebase Setup
+const firebase = require('firebase');
 firebase.initializeApp({
   serviceAccount: 'service-account.json'
 });
 
-var app = express();
+// Load config file
+const config = require('./config.json');
+
+// Generate Firebase Custom Auth token
+function generateFirebaseToken(lineMid) {
+  // The UID we'll assign to the user.
+  const firebaseUid = `line:${lineMid}`;
+
+  // Create the custom token.
+  const token = firebase.auth().createCustomToken(firebaseUid);
+  console.log('Created Custom token for UID "', firebaseUid, '" Token:', token);
+  return token;
+}
+
+// ExpressJS setup
+const app = express();
 app.use(bodyParser.json());
 
 // Verify LINE token and exchange for Firebase Custom Auth token
-app.post('/verifyToken', function(req, res) {
+app.post('/verifyToken', (req, res) => {
   if (req.body.token === undefined) {
-    res.status(400).send('Access Token not found');
-  } else {
-    var reqToken = req.body.token;
-    // console.log('LINE Access Token = ' + reqToken);
-
-    // Send request to LINE server for access token verification
-    var options = {
-      url: 'https://api.line.me/v1/oauth/verify',
-      headers: {
-        'Authorization': 'Bearer ' + reqToken
-      }
-    };
-    request(options, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // console.log(body);
-        var lineObj = JSON.parse(body);
-
-        if ((typeof lineObj.mid != 'undefined') && (lineObj.channelId == MY_LINE_CHANNEL_ID)) {
-          // Access Token Validation succeed with LINE server
-          // Generate Firebase token and return to device
-          var firebaseToken = generateFirebaseToken(lineObj.mid);
-
-          // Update Firebase user profile with LINE profile
-          updateUserProfile(reqToken, firebaseToken, lineObj.mid, function() {
-            var ret = {
-              firebase_token: firebaseToken
-            };
-            return res.status(200).send(ret);
-          });
-        }
-      } else {
-        var ret = {
-          error_message: 'Authentication error: Cannot verify access token.'
-        };
-        return res.status(403).send(ret);
-      }
-    });
-
+    return res.status(400).send('Access Token not found');
   }
+
+  const reqToken = req.body.token;
+  // console.log('LINE Access Token = ' + reqToken);
+
+  // Send request to LINE server for access token verification
+  const options = {
+    url: 'https://api.line.me/v1/oauth/verify',
+    headers: {
+      'Authorization': `Bearer ${reqToken}`
+    }
+  };
+  request(options, (error, response, body) => {
+    console.log(body);
+    if (!error && response.statusCode == 200) {
+      const lineObj = JSON.parse(body);
+
+      // Verify the token’s channelId match with my channelId to prevent spoof attack
+      if ((typeof lineObj.mid != 'undefined') && (lineObj.channelId == config.line.channelId)) {
+        // Access Token Validation succeed with LINE server
+        // Generate Firebase token and return to device
+        const firebaseToken = generateFirebaseToken(lineObj.mid);
+        const ret = {
+          firebase_token: firebaseToken
+        };
+        return res.status(200).send(ret);    
+      }
+    }
+      
+    const ret = {
+      error_message: 'Authentication error: Cannot verify access token.'
+    };
+    return res.status(403).send(ret);
+  });
+
 });
 
 // Endpoint to verify if your Node server is up
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   return res.status(200).send('Server is up and running!');
 });
 
 // Start the server
-var server = app.listen(process.env.PORT || '8080', function() {
+const server = app.listen(process.env.PORT || '8080', () => {
   console.log('App listening on port %s', server.address().port);
   console.log('Press Ctrl+C to quit.');
 });
